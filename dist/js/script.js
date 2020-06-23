d85e50152593ed2518cf569c721b8de1e552dbc5
Yuri Nemirich <nemirichyuri@gmail.com>
2020-06-23 21:38:37 +0300

    teams slider done, go icons fonts!

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
     $(".teams__carousel").slick({
         dots: false,
         infinite: true,
-        speed: 100,
+        speed: 1500,
+        autoplay: false,
+        autoplaySpeed: 2000,
         slidesToShow: 4,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/previus.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/next.png"></button>',
         slidesToScroll: 1,
         responsive: [{
                 breakpoint: 1024,
@@ -11,6 +15,8 @@ $(document).ready(function () {
                     slidesToShow: 3,
                     slidesToScroll: 3,
                     infinite: true,
+                    autoplay: true,
+                    autoplaySpeed: 2000,
                     dots: false,
                 },
             },
@@ -18,6 +24,8 @@ $(document).ready(function () {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 2,
+                    autoplay: true,
+                    autoplaySpeed: 2000,
                     slidesToScroll: 2,
                 },
             },
@@ -25,6 +33,8 @@ $(document).ready(function () {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
+                    autoplay: true,
+                    autoplaySpeed: 2000,
                     slidesToScroll: 1,
                 },
             },
